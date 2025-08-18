9bccc41cf8c4e332487c74153a665cb0f9148971
mentamaru <awaybeautman@icloud.com>
2025-08-18 13:03:23 +0900

    feat : 文字コード暗号化ウェブアプリ作成

---
+++ src/utils/xorEncryptDecrypt.ts
@@ -0,0 +1,23 @@
+// XOR暗号化・複合化関数
+export function xorEncryptDecrypt(text: string, key: string): string {
+  if (!key) return "";
+  let result = "";
+  const keyLength = key.length;
+  for (let i = 0; i < text.length; i++) {
+
+    // 暗号化する平文の文字コードを1文字分取得
+    const textCharCode = text.charCodeAt(i);
+
+    // 鍵文字の文字長に応じて、鍵文字の文字コードを取得
+    // 鍵文字が短い場合は、繰り返し使用する
+    // 例: 鍵文字が "abc" の場合、i=0なら 'a', i=1なら 'b', i=2なら 'c', i=3なら 'a' となる
+    const keyCharCode = key.charCodeAt(i % keyLength);
+
+    // 平文を鍵文字とXORビット演算し、暗号化する
+    const xorCharCode = textCharCode ^ keyCharCode;
+
+    // XOR演算の結果を文字コードに変換し、結果に追加
+    result += String.fromCharCode(xorCharCode);
+  }
+  return result;
+}
